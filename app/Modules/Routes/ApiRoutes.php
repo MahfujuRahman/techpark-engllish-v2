04525f163043e5478acb0af767f0d135684d1547
MahfujuRahman <akashajmain0@gmail.com>
2025-08-03 10:25:11 +0600

    boiler plate setup

---
+++ app/Modules/Routes/ApiRoutes.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Auth Management Module
+|--------------------------------------------------------------------------
+*/
+
+include_once base_path("app/Modules/Management/Auth/Routes/Route.php");
+/*
+|--------------------------------------------------------------------------
+| Dashboard data
+|--------------------------------------------------------------------------
+*/
+include_once base_path("app/Modules/Management/Dashboard/Routes/Route.php");
+/*
+|--------------------------------------------------------------------------
+| Setting Management Module
+|--------------------------------------------------------------------------
+*/
+
+include_once base_path("app/Modules/Management/SettingManagement/WebsiteSettings/Routes/Route.php");
+
+/*
+|--------------------------------------------------------------------------
+| User Management Module
+|--------------------------------------------------------------------------
+*/
+
+include_once base_path("app/Modules/Management/UserManagement/User/Routes/Route.php");
+include_once base_path("app/Modules/Management/UserManagement/Role/Routes/Route.php");
+/*
+|--------------------------------------------------------------------------
+| Others Management Module
+|--------------------------------------------------------------------------
+*/
