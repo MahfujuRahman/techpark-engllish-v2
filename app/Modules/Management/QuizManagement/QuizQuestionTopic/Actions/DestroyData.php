8020868db0bae16712a2dae1c6025ea182109150
MahfujuRahman <akashajmain0@gmail.com>
2025-08-13 15:15:59 +0600

    feat: Enhance data deletion process by ensuring related quiz questions and options are also deleted

--- app/Modules/Management/QuizManagement/QuizQuestionTopic/Actions/DestroyData.php
+++ app/Modules/Management/QuizManagement/QuizQuestionTopic/Actions/DestroyData.php
@@ -5,6 +5,10 @@ namespace App\Modules\Management\QuizManagement\QuizQuestionTopic\Actions;
 class DestroyData
 {
     static $model = \App\Modules\Management\QuizManagement\QuizQuestionTopic\Models\Model::class;
+    static $QuizQuestion = \App\Modules\Management\QuizManagement\QuizQuestion\Models\Model::class;
+    static $QuizQuestionOption = \App\Modules\Management\QuizManagement\QuizQuestion\Models\QuizQuestionOptionModel::class;
+
+    
 
     public static function execute($slug)
     {
@@ -13,6 +17,19 @@ class DestroyData
             if (!$data = self::$model::where('slug', $slug)->first()) {
                 return messageResponse('Data not found...', $data, 404, 'error');
             }
+
+            $QuizQuestion = self::$QuizQuestion::where('quiz_question_topic_id', $data->id)->get();
+
+            foreach ($QuizQuestion as $question) {
+                $question->forceDelete();
+            }
+
+            $QuizQuestionOption = self::$QuizQuestionOption::whereIn('quiz_question_id', $QuizQuestion->pluck('id'))->get();
+
+            foreach ($QuizQuestionOption as $option) {
+                $option->forceDelete();
+            }
+
             $data->forceDelete();
             return messageResponse('Item Successfully deleted', [], 200, 'success');
         } catch (\Exception $e) {
